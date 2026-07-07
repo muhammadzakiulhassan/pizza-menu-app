fa8ae5f3b500b251c460f8d920a6fe006920f6d3
Muhammad Zaki <muhammadzakiulhassan@users.noreply.github.com>
2026-07-07 17:30:05 +0500

    feat: apply styling to pizza-menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 
 const pizzaData = [
   {
@@ -48,32 +49,44 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
-     <Menu />
+      <Menu />
       <Footer />
     </div>
   );
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = { };
+//   const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>;
+  </header> 
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+</main>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  //   if (hour >= openHour && hour <= closeHour) alert("we're currently open!");
+  //   else alert("Sorry we're closed");
   return (
-    <footer>{new Date().toLocaleDateString()}.We're currently open</footer>
+    <footer className="footer">{new Date().toLocaleDateString()}.We're currently open</footer>
   );
   // return React.createElement('footer',null,'We are currently open')
 }
@@ -82,7 +95,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
